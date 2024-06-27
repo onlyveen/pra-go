d30d83298a351dcb8ffc9003962b7f5e17a83296
onlyveen <praveenasvenom@gmail.com>
2024-06-27 10:34:22 +0530

    oo

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs
 
-const nextConfig = {};
+const nextConfig = {
+  // Other Next.js configurations
+  output: {
+    // Set the directory for static export
+    dir: "out", // Replace 'out' with your desired export directory
+  },
+};
 
 export default nextConfig;
