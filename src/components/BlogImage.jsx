942571e2242f3a629bc454d10e48e0d89f510fb9
Praveen Gorakala <praveenasvenom@gmail.com>
2026-08-18 23:10:39 +0530

    Update BlogImage.jsx

--- src/components/BlogImage.jsx
+++ src/components/BlogImage.jsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const BlogImage = ({ src, alt }) => {
   const [loaded, setLoaded] = useState(false);
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    setLoaded(false);
+    if (imgRef.current?.complete) {
+      setLoaded(true);
+    }
+  }, [src]);
 
   return (
     <span className={`blog-image-wrap${loaded ? " is-loaded" : " is-loading"}`}>
-      <img src={src} alt={alt} loading="lazy" onLoad={() => setLoaded(true)} />
+      <img
+        ref={imgRef}
+        src={src}
+        alt={alt}
+        loading="lazy"
+        onLoad={() => setLoaded(true)}
+      />
     </span>
   );
 };
